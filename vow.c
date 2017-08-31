439f373541d647142ff9d8fbb56cd74d0ebb2d4d
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2017-08-31 20:00:54 +0530

    Update vow.c

--- vow.c
+++ vow.c
@@ -4,7 +4,7 @@ void main()
 char c;
 printf("Enter the character");
 scanf("%c",&c);
-if(c=='a'||c=='A'||c=='e'||c='E'||c=='o'||c=='O'||c=='i'||c=='I'||c=='u'||c=='U')
+if(c=='a'||c=='A'||c=='e'||c=='E'||c=='o'||c=='O'||c=='i'||c=='I'||c=='u'||c=='U')
 printf("Vowel");
 else
 printf("Consonent");
