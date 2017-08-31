65a6f88b7d8e26b8eaa9fc0bb653c60102d6eddc
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2017-08-31 22:24:24 +0530

    Update digit.c

--- digit.c
+++ digit.c
@@ -4,10 +4,11 @@ void main()
 int n,c=0;
 printf("Enter any number");
 scanf("%d",&n);
+m=n;
 while(n!=0)
 {
 n=n/10;
 c++;
 }
-printf("The digit of %d is:%d",n,c);
+printf("The digit of %d is:%d",m,c);
 }
