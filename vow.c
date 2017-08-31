36f6c10549feae698ae40a05f0dbf9d71a5a7269
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2017-08-31 19:57:29 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char c;
+printf("Enter the character");
+scanf("%c",&c);
+if(c=='a'||c=='A'||c=='e'||c='E'||c=='o'||c=='O'||c=='i'||c=='I'||c=='u'||c=='U')
+printf("Vowel");
+else
+printf("Consonent");
+}
