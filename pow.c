5f7c48212a43570c1f82957c649f4a1a9e71204d
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2017-09-02 07:36:08 +0530

    Create pow.c

---
+++ pow.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,b,n,i;
+printf("Enter the number");
+scanf("%d",&a);
+b=a;
+printf("Enter the power");
+scanf("%d",&p);
+for(i=1;i<=n-1;i++)
+{
+a=a*a;
+}
+printf("The power of %d is:%d",b,a);
+}
