5792cf6874c5c80a39a00d7488fe4ad5b6cb8269
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2017-09-15 21:24:39 +0530

    Update gcd2.py

--- gcd2.py
+++ gcd2.py
@@ -1,17 +1,17 @@
-x=int(input("Enter first number: "))
+num1=int(input("Enter first number: "))
  
-y=int(input("Enter second number: "))
-if x > y:
+num2=int(input("Enter second number: "))
+if num1 > num2:
  
-     smaller = y
+     smaller = num2
  
 else:
  
-    smaller = x
+    smaller = num1
  
     for i in range(1, smaller+1):
  
-         if((x % i == 0) and (y % i == 0)):
+         if((num1 % i == 0) and (num2 % i == 0)):
  
               hcf = i
 print "The H.C.F. of", x,"and", y,"is",hcf
