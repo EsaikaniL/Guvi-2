32aea288f9e82d9b628256e8c2734e7c8ee17712
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2017-09-24 19:09:54 +0530

    Create countingalpha.py

---
+++ countingalpha.py
@@ -0,0 +1,11 @@
+s=input('Enter any string')
+alp=0
+integer=0
+for x in s:
+	if x.isdigit():
+		integer=integer+1
+	elif x.isalpha():
+		alp=alp+1
+print 'The no of  integer in string is:',integer
+print 'The no of alpha in string is:',alp
+print 'The alpha numerical in string is:',integer+alp
