e44b348e6e75428ca23c71ef40466b82ed3bcaee
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2017-09-29 08:28:55 +0530

    Create fib2.py

---
+++ fib2.py
@@ -0,0 +1,13 @@
+n=int(input("Enter the term"))
+num1=0
+num2=1
+if n==1:
+  print num1
+else: 
+  print num1
+  print num2
+  for x in range(1,n-1):
+      n3=num1+num2
+      print n3
+      num1=num2
+      num2=num3
