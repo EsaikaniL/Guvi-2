4b126b71ae7f6cc976f8ef4fe9f2213be9278b29
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2017-09-29 07:36:06 +0530

    Create swap1.py

---
+++ swap1.py
@@ -0,0 +1,8 @@
+num1=int(input('Enter your 1st number'))
+num2=int(input('Enter your 2nd number'))
+print 'Befor swapping: \n',num1,num2
+print 'After swapping:\n'
+t=num1
+num1=num2
+num2=t
+print num1,num2
