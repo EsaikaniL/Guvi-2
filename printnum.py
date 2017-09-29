1baf4ec1b4cec9e7f1e28148bee34d8db1e52b09
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2017-09-29 14:44:50 +0530

    Create printnum.py

---
+++ printnum.py
@@ -0,0 +1,21 @@
+n=int(input('Enter any number:\n'))
+if(n==0):
+	print 'Zero'
+if(n==1):
+	print 'One'
+if(n==2):
+	print 'Two'
+if(n==3):
+	print 'Three'
+if(n==4):
+	print 'Four'
+if(n==5):
+	print 'Five'
+if(n==6):
+	print 'Six'
+if(n==7):
+	print 'Seven'
+if(n==8):
+	print 'Eight'
+if(n==9):
+	print 'Nine'
