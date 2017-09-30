58854eb54299e3578ddc31ea961b1155bfc701c1
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2017-09-30 12:11:56 +0530

    Create bigdiff.py

---
+++ bigdiff.py
@@ -0,0 +1,13 @@
+n=int(input('how many elements?'))
+s=[]
+t=[]
+for x in range(0,n):
+	num=int(input('Enter numbers:\n'))
+	s.append(num)
+for x in range(0,n-1):
+	y=s[x]-s[x+1]
+	if (y<0):
+		t.append(-y)
+	else:
+		t.append(y)
+print t
