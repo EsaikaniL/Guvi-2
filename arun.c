0ec41c991f5069f0f17010f578b5603100dba132
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2017-10-08 19:52:16 +0530

    Update arun.c

--- arun.c
+++ arun.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-char c;
+char d;
 printf("Enter the character");
-scanf("%c",c);
-if(c>='a'&&c<='z'||c>='A'&&c<='Z')
+scanf("%c",d);
+if(d>='a'&&d<='z'||d>='A'&&d<='Z')
 printf("Alphabet");
 else
 printf("Not alphabet");
