0d178998a322d449667f856a568696c4820fe9b7
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2017-10-12 21:20:52 +0530

    Create zigzagbinary.py

---
+++ zigzagbinary.py
@@ -0,0 +1,12 @@
+r=int(input('Enter the row\n'))
+s=['' for x in range(0,r)]
+st=''
+for x in range(0,r):
+	s[x]=raw_input()
+sd=list(s)
+for x in range(0,r):
+	if x%2==0:
+		st=st+s[x]
+	else:
+		st=st+(s[x][::-1])
+print st
