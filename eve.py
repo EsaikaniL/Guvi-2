90beebcc81c53b32bece48435822404da9acb24c
csbblabsystemvsb123 <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-01-09 17:01:07 +0530

    Create eve.py

---
+++ eve.py
@@ -0,0 +1,13 @@
+n1=int(input("How many rows in Graph:"))
+print("Enter your nodes from top to bottom")
+a=[]
+b=[]
+for x in range(0,n1):
+	t=input()
+	a.append(t)
+for x in a:
+	t=x[:-1]
+	b.append(t)
+print(b)
+for x in b:
+	print(x," ")
