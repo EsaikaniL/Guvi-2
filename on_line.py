9dfd494c908f166e674bb2d23a926d28bde7441b
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-01-19 07:55:26 +0000

    Create on_line.py

---
+++ on_line.py
@@ -0,0 +1,10 @@
+x1=int(input("X1 value:"))
+y1=int(input("Y1 value:"))
+x2=int(input("X2 value:"))
+y2=int(input("Y2 value:"))
+x3=int(input("X3 value:"))
+y3=int(input("Y3 value:"))
+if (x1==x2==x3) or (y1==y2==y3):
+    print("Yes")
+else:
+    print("No")
