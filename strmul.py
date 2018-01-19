e6f43dc7ea3a236fdbcce5c662b27ccce35468c9
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-01-19 21:34:39 +0530

    Update strmul.py

--- strmul.py
+++ strmul.py
@@ -1,4 +1,6 @@
-str1=raw_input('Enter 1st number:\n')
-str2=raw_input('Enter 2nd elemnt:\n')
-c=int(str1)*int(str2)
-print 'Multiplication of two number is:',c
+st=input("Enter the String:")
+ans=''
+for x in st:
+	k=ord(x)+3
+	ans=ans+chr(k)
+print(ans)
