308a28398c6f4f0e4f87645aeeedc25ce32fc117
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-01-19 21:17:31 +0530

    Update validip.py

--- validip.py
+++ validip.py
@@ -1,6 +1,8 @@
-st=raw_input('Enter your address:\n')
-str=list(st)
-str.insert(3,".")
-str.insert(7,".")
-str.insert(10,".")
-print ''.join(str)
+num1=int(input("Enter the 1st number:"))
+num2=int(input("Enter the 2nd number:"))
+a=[]
+if num1%num2==0 and num1%num1==0:
+	a.append(num1)
+if num2%num2==0 and num2%num1==0:
+	a.append(num2)
+print(min(a))
