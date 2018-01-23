6eb15ab216b7bd3785f88c85f24a75258d1bbf53
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-01-23 16:55:56 +0530

    Update sub_string.py

--- guvi/sub_string.py
+++ guvi/sub_string.py
@@ -2,12 +2,15 @@ s1=input("Enter the 1st string:")
 s2=input("Enter the 2nd string:")
 s=list(s1)
 flag=0
-for x in range(len(s)-1):
-	ans=''
-	for y in range(x,len(s)):
-		ans=ans+s[y]
-	if ans==s2:
-		flag=1
-		break
+for x in range(len(s)-2):
+	for y in range(len(s),x,-1):
+		ans=''
+		for z in range(x,y):
+			ans=ans+s[z]
+		if ans==s2:
+			flag=1
+			break
 if flag==1:
 	print("Yes")
+else:
+	print("No")
