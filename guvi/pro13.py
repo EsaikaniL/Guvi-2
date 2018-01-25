cc479d07345c311d9bcc4a23d97d551a40217576
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-01-25 23:33:59 +0530

    Create pro13.py

---
+++ guvi/pro13.py
@@ -0,0 +1,20 @@
+n=int(input("Enter N value:"))
+a=[]
+b=[]
+print("Enter Elements followed by N\n")
+for x in range(n):
+	k=int(input())
+	a.append(k)
+q=int(input("Enter the Q value:"))
+print("Enter Q elementsthe U and V value:")
+for x in range(q):
+	k=input()
+	b.append(k)
+for x in b:
+	k1=x.split(" ")[0].strip()
+	k2=x.split(" ")[1].strip()
+	ans=2000
+	for y in range(int(k1),int(k2)+1):
+		if ans>int(a[y]):
+			ans=int(a[y])
+	print(ans)
