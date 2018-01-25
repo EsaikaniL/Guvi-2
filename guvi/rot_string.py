c2951deaac1e493d56e702949a8c6dcee276b44b
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-01-25 16:06:39 +0530

    Create rot_string.py

---
+++ guvi/rot_string.py
@@ -0,0 +1,13 @@
+st=input("Enter the string:")
+n=int(input("How many rotation???"))
+s=list(st)
+ans=''
+for x in range(n):
+	temp=s[len(s)-1]
+	for y in range(len(s)-1,0,-1):
+		t=s[y]
+		s[y]=s[y-1]
+	s[0]=temp
+for x in s:
+	ans=ans+x
+print(ans)
