ae90507c2336e7df73fb0a60a09af9c652468792
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-01-28 14:24:55 +0530

    Update pro20.py

--- guvi/pro20.py
+++ guvi/pro20.py
@@ -6,13 +6,29 @@ for x in range(n):
 	a.append(k)
 s=int(input("Enter the sum value:"))
 b=sorted(a,reverse=True)
+b2=[]
+for x in range(len(b)-1):
+	for y in range(x+1,len(b)):
+		sum=b[x]+b[y]
+		b2.append(sum)
 res=0
 flag=0
 for x in b:
-	while(x<=s):
+	if s in b:
 		res+=1
-		s=s-x
 		flag=1
+		s=0
+		break
+	elif s in b2:
+		res+=2
+		flag=1
+		s=0
+		break
+	else:
+		while(x<=s):
+			res+=1
+			s=s-x
+			flag=1
 if s==0 and flag==1:
     print(res)
 else:
