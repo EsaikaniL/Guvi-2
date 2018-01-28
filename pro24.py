fbdce5ff6f80409d3a662364c8aee080da281fa7
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-01-28 21:41:26 +0530

    Create pro24.py

---
+++ pro24.py
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+	int k;
+	int i,num=1,rem,base,bin,b;
+	printf("Enter k value: \n");
+	scanf("%d",&k);
+	for(i=0;i<k;i++)
+	{
+		num=num*2;
+	}
+	printf("The %d digit binary values:\n",k);
+	for(i=0;i<num;i++)
+	{
+		b=i;
+		rem=0,base=1,bin=0;
+		while(b!=0)
+		{
+			rem=b%2;
+			bin=bin+rem*base;
+			base=base*10;
+			b=b/2;
+		}
+		printf("%d \n",bin);
+	}
+}
+© 2018 GitHub, Inc.
