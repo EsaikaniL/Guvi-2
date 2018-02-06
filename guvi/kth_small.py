cbc3ce5f64b65211740d8d02a15ce8c330662b0c
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-02-06 15:52:19 +0530

    Create kth_small.py

---
+++ guvi/kth_small.py
@@ -0,0 +1,10 @@
+n=int(input("How many elements??"))
+print("Enter th elements....")
+a=[]
+for x in range(n):
+	k=int(input())
+	a.append(k)
+a.sort()
+print(a)
+p=int(input("Enter the k value:"))
+print(p,"st smallest number is:",a[p-1])
