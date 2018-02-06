ac176399814e285855c8ccf5a0a3dd0063b8d67e
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-02-06 16:24:01 +0530

    Update occur_word.py

--- guvi/occur_word.py
+++ guvi/occur_word.py
@@ -1,12 +1,8 @@
 st=input("Enter the line")
 a=st.split(" ")
 st2=input("Enter the string:")
-flag=0
+i=0
 for x in a:
 	if x==st2:
-		flag=1
-		break
-if  flag==0:
-	print("No")
-else:
-	print("Yes")
+		i+=1
+print(i)
