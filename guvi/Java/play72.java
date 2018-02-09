cd3fc1025208b4c139131e9432a195a7ad6ce7e3
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-02-09 15:12:48 +0530

    Create play72.java

---
+++ guvi/Java/play72.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class play72
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	ArrayList a=new ArrayList();
+	int i,n,k,p;
+	Scanner in=new Scanner(System.in);
+	n=in.nextInt();
+	for(i=0;i<n;i++)
+	{
+		k=in.nextInt();
+		a.add(k);
+	}
+	Collections.sort(a);
+	
+	System.out.println(a);
+	}
+}
