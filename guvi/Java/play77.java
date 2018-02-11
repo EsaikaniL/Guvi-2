aae1166eaef09a167749d85e3c41910fc935d0c1
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-02-11 10:36:31 +0530

    Create play77.java

---
+++ guvi/Java/play77.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class play77
+{
+	public static void main (String[] args)
+	{
+	int a[]=new int[20];
+		int n,i,j,ct,max,ans=0;
+		Scanner in=new Scanner(System.in);
+		System.out.println("How many elements??");
+		n=in.nextInt();
+			System.out.println("\nEnter the elements..");
+		for(i=0;i<n;i++)
+		{
+				a[i]=in.nextInt();
+		}
+		for(i=0;i<n-1;i++)
+		{
+			max=a[i];
+			ct=0;
+			for(j=i+1;j<n;j++)
+			{
+				if(a[i]<a[j])
+				{
+					ct++;
+				}
+				
+			}
+			if(ct>ans)
+			{
+				ans=ct;
+			}
+		}
+		System.out.println(ans);
+	}
+}
