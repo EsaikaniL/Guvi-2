f990df8c92a39cd8026334206c3c9e24799d39fe
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-02-11 09:59:37 +0530

    Create play75.java

---
+++ guvi/Java/play75.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class play75
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a[]=new int[20];
+		int n,i,j,ct=0;
+		Scanner in=new Scanner(System.in);
+		System.out.println("How many elements??");
+		n=in.nextInt();
+			System.out.println("\nEnter the elements..");
+		for(i=0;i<n;i++)
+		{
+				a[i]=in.nextInt();
+		}
+		for(i=0;i<n-1;i++)
+		{
+			for(j=i+1;j<n;j++)
+			{
+				if(a[i]<a[j])
+				{
+					ct++;
+				}
+			}
+		}
+		System.out.println(ct);
+	}
+}
