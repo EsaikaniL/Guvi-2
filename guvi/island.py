db0dff502bc5864935a17ed4c19e0f4dd2f1b19b
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-02-16 16:36:54 +0530

    Update island.py

--- guvi/island.py
+++ guvi/island.py
@@ -6,12 +6,24 @@ for x in range(0,n):
 	for y in range(n):
 		array[x][y]=int(input())
 for x in range(0,n):
-	for y in range(1,n-1):
-		if array[x][y]==1:
-			if (x==0)and (array[x+1][y]==0 and array[x][y-1]==0 and array[x][y+1]):
-				count=count+1
-			elif (x==n-1)and (array[x-1][y]==0 and array[x][y-1]==0 and array[x][y+1]):
-				count=count+1
-			elif x!=0 or x!=n-1 and (array[x+1][y]==0 and array[x][y-1]==0 and array[x][y+1] and array[x-1][y]==0):
-				count=count+1
-print("Island",count)
+    for y in range(0,n):
+            if array[x][y]==1:
+                if x==0 and y==0 and array[x+1][y]==0 and array[x][y+1]==0:
+                    count+=1
+                elif(x==0 and y==n-1) and array[x+1][y]==0 and array[x][y-1]==0:
+                    count+=1
+                elif(x==n-1and y==0) and array[x-1][y]==0 and array[x][y+1]==0:
+                    count+=1
+                elif(x==n-1and y==n-1) and array[x-1][y]==0 and array[x][y-1]==0:
+                    count+=1
+                elif(x==0and y!=0 and y!=n-1) and array[x+1][y]==0 and array[x][y+1]==0 and array[x][y-1]==0:
+                    count+=1
+                elif(x==n-1and y!=0 and y!=n-1) and array[x-1][y]==0 and array[x][y+1]==0 and array[x][y-1]==0:
+                    count+=1
+                elif(y==0and x!=0 and x!=n-1) and array[x+1][y]==0 and array[x-1][y]==0 and array[x][y+1]==0:
+                    count+=1
+                elif(y==n-1and x!=0 and x!=n-1) and array[x+1][y]==0 and array[x-1][y]==0 and array[x][y-1]==0:
+                    count+=1
+                elif array[x][y-1]==0 and array[x][y+1]==0 and array[x+1][y]==0 and array[x-1][y]==0:
+                    count+=1
+print("Island",count)                    
