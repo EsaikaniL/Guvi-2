0fd13fe77197fd6e39e8473d6a6b1625540bf0fb
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-02-16 16:38:11 +0530

    Update island.py

--- guvi/island.py
+++ guvi/island.py
@@ -12,17 +12,17 @@ for x in range(0,n):
                     count+=1
                 elif(x==0 and y==n-1) and array[x+1][y]==0 and array[x][y-1]==0:
                     count+=1
-                elif(x==n-1and y==0) and array[x-1][y]==0 and array[x][y+1]==0:
+                elif(x==n-1 and y==0) and array[x-1][y]==0 and array[x][y+1]==0:
                     count+=1
-                elif(x==n-1and y==n-1) and array[x-1][y]==0 and array[x][y-1]==0:
+                elif(x==n-1 and y==n-1) and array[x-1][y]==0 and array[x][y-1]==0:
                     count+=1
-                elif(x==0and y!=0 and y!=n-1) and array[x+1][y]==0 and array[x][y+1]==0 and array[x][y-1]==0:
+                elif(x==0 and y!=0 and y!=n-1) and array[x+1][y]==0 and array[x][y+1]==0 and array[x][y-1]==0:
                     count+=1
-                elif(x==n-1and y!=0 and y!=n-1) and array[x-1][y]==0 and array[x][y+1]==0 and array[x][y-1]==0:
+                elif(x==n-1 and y!=0 and y!=n-1) and array[x-1][y]==0 and array[x][y+1]==0 and array[x][y-1]==0:
                     count+=1
-                elif(y==0and x!=0 and x!=n-1) and array[x+1][y]==0 and array[x-1][y]==0 and array[x][y+1]==0:
+                elif(y==0 and x!=0 and x!=n-1) and array[x+1][y]==0 and array[x-1][y]==0 and array[x][y+1]==0:
                     count+=1
-                elif(y==n-1and x!=0 and x!=n-1) and array[x+1][y]==0 and array[x-1][y]==0 and array[x][y-1]==0:
+                elif(y==n-1 and x!=0 and x!=n-1) and array[x+1][y]==0 and array[x-1][y]==0 and array[x][y-1]==0:
                     count+=1
                 elif array[x][y-1]==0 and array[x][y+1]==0 and array[x+1][y]==0 and array[x-1][y]==0:
                     count+=1
