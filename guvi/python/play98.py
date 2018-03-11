2819de85ec9266906d9cc8859fbd8ff36c58a5da
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-03-11 10:28:14 +0530

    Update play98.py

--- guvi/python/play98.py
+++ guvi/python/play98.py
@@ -2,10 +2,10 @@ n=input()
 k=int(input())
 a=[]
 c=0
-for x in range(0,k+1):
-    a.append(x)
 for x in n:
-    if int(x) not in a:
+    a.append(int(x))
+for x in range(0,k+1):
+    if x not in a:
         c=1
 if c==0:
     print("Yes")
