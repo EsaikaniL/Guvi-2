0b3ab1f293d73aed20eee0160c87f20713e599ef
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-03-11 16:24:14 +0530

    Update not_divise.py

--- not_divise.py
+++ not_divise.py
@@ -1,7 +1,7 @@
-num=int(input("Enter the Number:"))
-while(num!=0):
-	if num%2==0:
-		num=num//2
+num1=int(input("Enter the Number:"))
+while(num1!=0):
+	if num1%2==0:
+		num1=num1//2
 	else:
 		break
-print(num)
+print(num1)
