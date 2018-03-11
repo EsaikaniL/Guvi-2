2be4918053eaa14cdeac9def11a2a794772f8450
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-03-11 20:08:55 +0530

    Create play116.py

---
+++ guvi/python/play116.py
@@ -0,0 +1,15 @@
+n=int(input())
+a=input().strip().split(" ")
+b=[]
+ans=""
+for x in range(n-1):
+    for y in range(x+1,n):
+        if a[x]==a[y] and a[x] not in b:
+            b.append(a[x])
+b.sort()
+for x in b[::-1]:
+    ans+=str(x)+" "
+for x in a:
+    if x not in b:
+         ans+=str(x)+" "
+print(ans.strip())
