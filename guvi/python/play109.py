f80c9bcc77fede6e1717dc5d6204ea8a077a5f65
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-03-11 16:08:24 +0530

    Update play109.py

--- guvi/python/play109.py
+++ guvi/python/play109.py
@@ -3,9 +3,10 @@ k=input().strip()
 a=k.split(" ")
 ans=""
 p=0
+a.sort()
 for x in a:
     p+=int(x)
-for x in a:
+for x in a[::-1]:
     ans=ans+str(p)+" "
-    p=p-int(x)
-print(ans.strip())
+    p-=int(x)
+print(ans)
