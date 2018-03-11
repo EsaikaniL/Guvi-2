ef3f0c7602587536824ab701d8ed5ab7be291f0a
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-03-11 16:48:40 +0530

    Create play111.py

---
+++ guvi/python/play111.py
@@ -0,0 +1,21 @@
+ip=input().strip()
+n=int(ip.split(" ")[0])
+m=int(ip.split(" ")[1])
+st=input()
+a=st.split(" ")
+ans=""
+b=[]
+c=[]
+d=[]
+for x in range(n):
+    b.append(a[x])
+for x in range(m):
+    c.append(a[x])
+for x in b:
+    for y in c:
+        if x==y:
+            if x not in d:
+                d.append(x)
+for x in d:
+    ans+=str(x)+" "
+print(ans.strip())
