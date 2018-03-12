aa52ee5dfc1167b3e1ee7c20d73c3c4e0a3e50bd
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-03-12 21:01:21 +0530

    Create hunt54.py

---
+++ guvi/python/hunt54.py
@@ -0,0 +1,11 @@
+n=int(input())
+a=input().strip().split(" ")
+ans=""
+mi=1000000
+for x in a:
+    if int(x)<mi:
+        mi=int(x)
+        ans+=x+" "
+    else:
+        ans+=str(mi)+" "
+print(ans.strip())
