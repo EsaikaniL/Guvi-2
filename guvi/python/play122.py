ba45380a5869cfdc27982fa84755615e6d4ae441
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-03-13 16:53:06 +0530

    Update play122.py

--- guvi/python/play122.py
+++ guvi/python/play122.py
@@ -6,7 +6,7 @@ elif int(st[1])==2:
 elif int(st[1])==3:
     print("March")
 elif int(st[1])==4:
-    print("April ")
+    print("April")
 elif int(st[1])==5:
     print("May")
 elif int(st[1])==6:
@@ -18,7 +18,7 @@ elif int(st[1])==8:
 elif st[1]==9:
     print("September")
 elif int(st[1])==10:
-    print("October ")
+    print("October")
 elif int(st[1])==11:
     print("November")
 elif int(st[1])==12:
