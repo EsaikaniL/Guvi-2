a7567ff2074c5ce6d2e62881edfb8ef4ca01d67b
Muthukumar M <30829540+csbblabsystemvsb123@users.noreply.github.com>
2018-03-13 16:50:24 +0530

    Create play122.py

---
+++ guvi/python/play122.py
@@ -0,0 +1,25 @@
+st=input().strip().split("-")
+if int(st[1])==1:
+    print("January")
+elif int(st[1])==2:
+    print("February")
+elif int(st[1])==3:
+    print("March")
+elif int(st[1])==4:
+    print("April ")
+elif int(st[1])==5:
+    print("May")
+elif int(st[1])==6:
+    print("June")
+elif int(st[1])==7:
+    print("July")
+elif int(st[1])==8:
+    print("August")
+elif st[1]==9:
+    print("September")
+elif int(st[1])==10:
+    print("October ")
+elif int(st[1])==11:
+    print("November")
+elif int(st[1])==12:
+    print("December")
